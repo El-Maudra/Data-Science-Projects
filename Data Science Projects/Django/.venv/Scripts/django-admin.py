67b85ad4e059ec944e43939e831c38533c7b7926
Walter Nathaniel <wltr.nathaniel@gmail.com>
2024-04-12 13:39:28 +0300

    Delete Data Science Projects/Django/.venv directory

--- Data Science Projects/Django/.venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Walter Owando\Documents\GitHub\Data-Science-Projects\Data Science Projects\Django\.venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
